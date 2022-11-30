5de9a6ad9ff368c3d1ba530f7bffc95250084bd8
Erik Verduin <everduin@cisco.com>
2022-11-30 17:23:13 -0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteInspector } from './custom-plugins/inspector/plugin';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [ sveltekit(), svelteInspector()],
+};
+
+export default config;
